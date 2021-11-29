97dd0ed2e15cadcf3fc1d142f8c11c882acf4529
Luis Jorge <gevolutionxalapa@gmail.com>
2021-11-29 10:49:47 -0600

    Alinear el contenido con el flexbox de react nactive

--- App.js
+++ App.js
@@ -39,31 +39,40 @@ const styles = StyleSheet.create({
     // Cambias la direccion del flex box por default estan en columnas
     // flexDirection: 'row',
     // flexDirection: 'row-reverse',
-    // flexDirection: 'columns', // defualt
+    flexDirection: 'column',
     // flexDirection: 'column-reverse',
+    // Jutificacion cuando esta en modo row
+    // justifyContent: 'flex-end',
+    // justifyContent: 'flex-start',
+    // justifyContent: 'center',
+    // justifyContent: 'space-between',
+    // Alineacion del contenido ya sea arriba o abajo
+    // alignItems: 'flex-start',
+    // alignItems: 'flex-end',
+    alignItems: 'center',
   },
   caja1: {
     padding: 20,
     backgroundColor: 'navy',
     // Indica que tomara todo el espacio sobrante dependiente del padre
-    flex: 1,
+    // flex: 1,
   },
   caja2: {
     padding: 20,
     backgroundColor: 'yellow',
     // Tomara el espacio propocional
-    flex: 1,
+    // flex: 1,
   },
   caja3: {
     padding: 20,
     backgroundColor: 'green',
-    flex: 1,
+    // flex: 1,
   },
   caja4: {
     padding: 20,
     backgroundColor: 'teal',
     // Tomara mayour parte proporcional
-    flex: 3,
+    // flex: 3,
   },
 });
 export default App;
